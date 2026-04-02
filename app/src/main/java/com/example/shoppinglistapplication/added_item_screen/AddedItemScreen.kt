a14e50ae13ab9ca1947e9df9eeb506a156397887
Sano <sano.tsarevich@gmail.com>
2026-04-02 18:35:02 +0800

    add added screen UI

--- app/src/main/java/com/example/shoppinglistapplication/added_item_screen/AddedItemScreen.kt
+++ app/src/main/java/com/example/shoppinglistapplication/added_item_screen/AddedItemScreen.kt
@@ -1,11 +1,111 @@
 package com.example.shoppinglistapplication.added_item_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.IconButton
+import androidx.compose.material.ModalDrawer
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shoppinglistapplication.R
+import com.example.shoppinglistapplication.dialog.DialogController
+import com.example.shoppinglistapplication.dialog.MainAlertDialog
+import com.example.shoppinglistapplication.ui.theme.Orange
+import com.example.shoppinglistapplication.ui.theme.White_2
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun AddItemScreen(
     viewModel: AddedItemViewModel = hiltViewModel()
-) {
+){
+    val scaffoldState = rememberScaffoldState()
+    val itemsList = viewModel.itemsList?.collectAsState(
+        initial = emptyList()
+    )
 
+    Scaffold(
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .background(White_2)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextField(
+                        modifier = Modifier.weight(1f),
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(
+                                text = "New item:",
+                                fontSize = 12.sp
+                            )
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        textStyle = TextStyle(
+                            fontSize = 16.sp,
+                            color = Color.Black
+                        ),
+                        singleLine = true
+                    )
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_add),
+                            contentDescription = ""
+                        )
+                    }
+                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp)
+            ) {
+                if(itemsList != null) {
+                    items(itemsList.value) {
+                        UI_AddedScreenItemCard(item = it, onEvent = {})
+                    }
+                }
+            }
+        }
+        MainAlertDialog(viewModel)
+    }
 }
